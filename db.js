3611b06997b882e0e74ba4935ecd749eed6aa4bd
3mirSahin <es3hd@virginia.edu>
2023-01-22 22:08:44 +0300

    Persistent db and moves

--- db.js
+++ db.js
@@ -1,7 +1,18 @@
 let db;
-let moves = new Set(['Deadlift', 'Benchpress', 'Squat']);
+let moves = new Set(JSON.parse(localStorage.getItem("movesSet")) || ['Deadlift', 'Benchpress', 'Squat']);
 let dbReq = indexedDB.open('Workouts', 1);
-let movesDrop = document.getElementById("selectNumber");
+let movesDrop = document.getElementById("selectExercise");
+let setForm = document.getElementById("Set Form");
+let formButton = document.getElementById("buttonSetForm")
+
+
+//add the current options to the moves list
+for (let option of moves){
+  let newOption = document.createElement("option");
+  newOption.value = option;
+  newOption.text = option;
+  movesDrop.appendChild(newOption);
+}
 
 dbReq.onupgradeneeded = function(event) {
   // Set the db variable to our database so we can use it!  
@@ -11,18 +22,30 @@ dbReq.onupgradeneeded = function(event) {
   // in databases are where data are stored.
   // let moves = db.createObjectStore('moves', {autoIncrement: true});
   // let workouts = db.createObjectStore('workouts', {autoIncrement: true});
-  let moveHist = db.createObjectStore('history', {autoIncrement: true});
-
+  let moveHist = db.createObjectStore('history', {keyPath: 'id', autoIncrement: true});
 }
+
 dbReq.onsuccess = function(event) {
   db = event.target.result;
   // moves.add(db);
   // moves.set()
 }
 
+
 dbReq.onerror = function(event) {
   alert('error opening database ' + event.target.errorCode);
 }
+
+formButton.addEventListener("click", function(event) {
+  event.preventDefault();
+  if (!setForm.reportValidity()){
+    event.preventDefault();
+    alert("Fill in the required fields.")
+  }
+  else{
+    submitSet();
+  }
+});
 
 // function addMove(db, message){
 //     //Start transaction and get workouts storage
@@ -40,15 +63,22 @@ dbReq.onerror = function(event) {
 //         alert('error saving move ' + event.target.errorCode);
 //     }
 // }
+
+
 function addHist(db, repCount, weight, move){
   //Start transaction and get workouts storage
   let tx = db.transaction(['history'], 'readwrite');
   let store = tx.objectStore('history');
 
+  //add set to database
+  let moveSet = {
+    exer : move,
+    reps : repCount,
+    kg : weight,
+    created: new Date().getTime(),
+  };
+  store.add(moveSet);
 
-  //add workout to database
-  let set = {reps: repCount, kg: weight, moveKey: move};
-  store.add(set);
 
   //wait for transaction to be done
   tx.oncomplete = function() {console.log('New set saved!')}
@@ -56,26 +86,31 @@ function addHist(db, repCount, weight, move){
     alert('Error saving set.');
   }
 }
+
 function submitMove(){
-  let message = document.getElementById("newmessage");
-  moves.add(message.value);
-  // remove everything in the dropdown
-  movesDrop.innerHTML = "";
-
-  //Loop over each element and add it to the dropdown
-  for (let option of moves){
+  let message = document.getElementById("newMove");
+  if (!moves.has(message.value)) {
+    moves.add(message.value);
+    //add the move to the moves dropdown
     let newOption = document.createElement("option");
-    newOption.value = option;
-    newOption.text = option;
+    newOption.value = message.value;
+    newOption.text = message.value;
     movesDrop.appendChild(newOption);
+    localStorage.setItem("movesSet",JSON.stringify([...moves]))
+    alert("Added move.")
   }
+  else{
+    alert("Move is a duplicate.")
+  }
+  
+}
 
-}
 function submitSet(){
-  let message = document.getElementById("newmessage");
-  addMove(db,message.value);
-  message.value = '';
-}
-function createWorkout(){
-  //Adds a new workout and creates a key for it
+  let move = document.getElementById("selectExercise").value;
+  let wgh = document.getElementById("weightIn").value;
+  let rep = document.getElementById("repIn").value;
+  if (!move == ""){
+    addHist(db, rep,wgh,move);
+
+  }
 }
